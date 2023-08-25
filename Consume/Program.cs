4af6799a6d3359fefa9bb90c69c3bafd19e510c1
Lars Fjermeros <larsfjermeros@gmail.com>
2023-08-25 17:41:44 +0200

    Add projects

---
+++ Consume/Program.cs
@@ -0,0 +1,41 @@
+﻿using EventStore.Client;
+
+using var client = new EventStoreClient(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
+var count = 0;
+Position checkpoint = default;
+StreamSubscription? subscription = default;
+
+await Consume(FromAll.End);
+
+async Task Consume(FromAll position) 
+{
+    Console.WriteLine("Consuming...");
+
+    subscription = await client.SubscribeToAllAsync(
+        position,
+        (_, resolved, ct) => Appeared(resolved),
+        subscriptionDropped: (_, reason, exc) => SubscriptionDropped(reason, exc));
+}
+
+Task Appeared(ResolvedEvent evt)
+{
+    count++;
+    checkpoint = evt.Event.Position;
+    return Task.CompletedTask;
+}
+
+Task SubscriptionDropped(SubscriptionDroppedReason reason, Exception? exception)
+{
+    Console.WriteLine("SubscriptionDropped");
+    Console.WriteLine($"Events Handled Count: {count}");
+
+    subscription?.Dispose();
+
+    if (reason != SubscriptionDroppedReason.Disposed)
+        return Consume(FromAll.After(checkpoint));
+
+    return Task.CompletedTask;
+}
+
+Console.ReadLine();
+Console.WriteLine($"Total Events Handled Count: {count}");
---
+++ Produce/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Buffers;
+using EventStore.Client;
+
+using var client = new EventStoreClient(EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"));
+using var bufferPool = MemoryPool<byte>.Shared;
+var random = new Random();
+
+const int MAX_EVENT_SIZE = 256;
+
+while (true) 
+{
+    Console.WriteLine("Producing...");
+
+    var tasks = new List<Task>();
+    for (var n = 1; n <= 10; n++) 
+    {
+        tasks.Add(Produce($"n{n}"));
+    }
+
+    await Task.WhenAll(tasks);
+    await Task.Delay(1_000);
+}
+
+async Task Produce(string streamNamespace, int streamsCount = 10, int eventsPerStream = 5) 
+{
+    var tasks = new Task[streamsCount];
+
+    for (var i = 0; i < tasks.Length; i++) 
+    {
+        var streamName = $"test.{streamNamespace}-{i+1}";
+        var events = new EventData[eventsPerStream];
+
+        for (var j = 0; j < events.Length; j++) 
+        {
+            events[j] = GenerateRandomEventData(eventType: $"TestEvent{j+1}");
+        }
+
+        tasks[i] = client.AppendToStreamAsync(streamName, StreamState.Any, events);
+    }
+
+    await Task.WhenAll(tasks);
+
+    EventData GenerateRandomEventData(string eventType, string contentType = "application/octet-stream") 
+    {
+
+        var eventId = Uuid.NewUuid();
+
+        var size = random.Next(MAX_EVENT_SIZE);
+        var buffer = bufferPool.Rent(size);
+        var data = buffer.Memory[..size];
+        random.NextBytes(data.Span);
+
+        return new EventData(eventId, eventType, data, contentType: contentType);
+    }
+}
